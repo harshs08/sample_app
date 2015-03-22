761b18faadb685863c15a961c120a4b880a69f0b
Harsh Singh <harshsin@usc.edu>
2015-03-22 01:42:50 -0700

    adds the test for failure login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,21 @@
 class SessionsController < ApplicationController
-  
-  def new
-  end
 
-  def create
-  	user = User.find_by(email: params[:session][:email].downcase)
-  	if user && user.authenticate(params[:session][:password])
-  		# Log the user in and redirect to the user's show page.
-  	else
-  		flash[:danger] = 'Invalid email/password combination'
-  		render 'new'
-  	end
+	def new
+	end
 
-  end
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			# Log the user in and redirect to the user's show page.
+		else
+			flash.now[:danger] = 'Invalid email/password combination'
+			render 'new'
+		end
 
-  def destroy
+	end
 
-  end
+	def destroy
+
+	end
 
 end
