c6623c053dcfb1d14b7e3a4148a82a0db0d02418
Harsh Singh <harshsin@usc.edu>
2015-04-25 20:21:16 -0700

    adds the email integration tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -74,6 +74,11 @@ class User < ActiveRecord::Base
 		UserMailer.password_reset(self).deliver_now
 	end
 
+  # Returns true if a password reset has expired.
+	def password_reset_expired?
+		reset_sent_at < 2.hours.ago
+	end
+
 	private
 		# Creates and assigns the activation token and digest.
 		def create_activation_digest
